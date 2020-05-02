dd0da31f3652a3bb28e04a1989511f557e8510c2
tagyoureit <russ.goldin@gmail.com>
2020-05-02 08:03:57 -0700

    refactor; additional options

--- config.tsx
+++ config.tsx
@@ -1,4 +1,4 @@
 // this can now be set from the UI
 // however, it is set to false by default so if debug is needed when the extension is first loading
 // then set this to true and it will override the UI options setting
-export const debugOverride=true;
+export const debugOverride=false;
